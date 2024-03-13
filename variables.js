55a1aad662f444639269c18949090032d99c73b0
Dalakoti01 <140832730+Dalakoti01@users.noreply.github.com>
2024-03-13 20:44:32 +0000

    conainer configured

---
+++ variables.js
@@ -0,0 +1,14 @@
+const name = "Karan";
+let salary = "100000"
+var city = "delhi"
+state = "UTTARPRADESH" // this way of declaring variables is not recommended
+
+// name = "sagar" // not allowed
+
+salary = "2000000"
+city = "agra"
+
+console.table([name,salary,city,state])
+
+// prefer not to use var 
+// because of problems in block scope and functional scope 
