dc6fe4506305c1b4d2c21097b52c9381bc357c92
Dalakoti01 <140832730+Dalakoti01@users.noreply.github.com>
2024-03-14 18:27:11 +0000

    datatypes

---
+++ .devcontainer/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version
+
+console.log(3 +
+    3);// This code is executable but the readability of this code is very less 
+
+
+// Datatypes:
+let name = "karan"// string data type 
+
+let age = 20 //number datatype
+let isLoggedIn = true; //boolean datatype
+
+//number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true / false
+// null => standalone value
+// undefine => let name;
+// symbol => this will used more during react to know the uniqueness (figma tools)
+// object 
+
+console.log(typeof "karan");
+console.log(typeof null); // object is the output in type of null . This thing is  also considered as a limitation of javascript
+console.log("karan")
+
